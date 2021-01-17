a7fd99c0655340d4e6c34f46a195b073e54b71a3
Parsa KamaliPour <parsakamalipour@gmail.com>
2021-01-17 20:13:29 +0330

    not done

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Vector3 movement = new Vector3();
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
